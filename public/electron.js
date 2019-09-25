1b239f427d38a181a0e5fa35f651821173512523
Sandro Jikia <jikiasandro@gmail.com>
2019-09-26 02:01:58 +0400

    Added tray

--- public/electron.js
+++ public/electron.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const isDev = require('electron-is-dev');
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray,  Menu} = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+
+let tray;
+
+function init() {
+  createWindow();
+  createTray();
+}
 
 function createWindow () {
   // Create the browser window.
@@ -32,17 +39,37 @@ function createWindow () {
   })
 }
 
+function createTray() {
+  tray = new Tray(__dirname + '/favicon.ico');
+
+  const contextMenu = Menu.buildFromTemplate([{
+    label: 'Quit',
+    click() {
+      app.quit();
+    }
+  }]);
+  tray.setContextMenu(contextMenu);
+  tray.setToolTip('Clipman');
+
+  tray.on('click', () => {
+    if (win)
+      win.show();
+    else
+      createWindow();
+  });
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', init);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    //app.quit()
   }
 });
 
@@ -50,6 +77,6 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
 });
